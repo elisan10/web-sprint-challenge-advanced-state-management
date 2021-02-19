cca40004efc7f5ea0ae823aad0286900e6845b25
Eli <elize.san10@gmail.com>
2021-02-19 09:17:50 -0600

    index.js  - all necessary components and library methods added  - store created that includes thunk and logger  - Provider element is wrapped around App component
    reducers/index.js
     - initial state created with values

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,24 @@
-
 export const initialState = {
-}
+  smurfs: [
+    {
+      id: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+      name: "Poppa Smurf",
+      position: "Village Leader",
+      nickname: "Pops",
+      description:
+        "Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.",
+    },
+  ],
+  isLoading: false,
+  error: "",
+};
 
-const reducer = ()=>{
-}
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
 
 export default reducer;
 
